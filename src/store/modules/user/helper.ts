cd72eabdef3d33dc4e2b3c7b0b191df9f1ba20fd
aishui8633 <aishui8633@hotmail.com>
2023-04-29 19:17:30 +0800

    提交文件

---
+++ src/store/modules/user/helper.ts
@@ -0,0 +1,32 @@
+import { ss } from '@/utils/storage'
+
+const LOCAL_NAME = 'userStorage'
+
+export interface UserInfo {
+  avatar: string
+  name: string
+  description: string
+}
+
+export interface UserState {
+  userInfo: UserInfo
+}
+
+export function defaultSetting(): UserState {
+  return {
+    userInfo: {
+      avatar: 'https://raw.githubusercontent.com/aishui8633/ChatGPT/main/src/assets/avatar.jpg',
+      name: '个人小秘书',
+      description: '',
+    },
+  }
+}
+
+export function getLocalState(): UserState {
+  const localSetting: UserState | undefined = ss.get(LOCAL_NAME)
+  return { ...defaultSetting(), ...localSetting }
+}
+
+export function setLocalState(setting: UserState): void {
+  ss.set(LOCAL_NAME, setting)
+}
